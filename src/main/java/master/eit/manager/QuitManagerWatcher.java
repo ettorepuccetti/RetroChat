9dd58b9c5a3fe05b8d0130e0af50a2e1ca053128
Ettore <puccio-94@live.it>
2018-10-31 12:44:33 +0100

    quit manager part

---
+++ src/main/java/master/eit/manager/QuitManagerWatcher.java
@@ -0,0 +1,48 @@
+package master.eit.manager;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
+
+
+
+public class QuitManagerWatcher implements Watcher, Runnable {
+
+    ZooManager zm;
+    CountDownLatch onetime = new CountDownLatch(1);
+
+
+    public QuitManagerWatcher(ZooManager zoo) {
+        this.zm = zoo;
+        System.out.println("Manager Quit Watcher set");
+
+    }
+
+    public void process(WatchedEvent we) {
+        if (we.getType() == EventType.NodeChildrenChanged) {
+            System.out.println("Manager Watcher triggered !!");
+            try {
+                zm.quit();
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        onetime.countDown();
+    }
+
+    public void run() {
+        synchronized(this) {
+            try {
+                onetime.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Manager Watcher Quit thread ends");
+        }
+    }
+}
--- src/main/java/master/eit/manager/ZooManager.java
+++ src/main/java/master/eit/manager/ZooManager.java
@@ -67,10 +67,10 @@ public class ZooManager implements Runnable {
                     byte[] bdata = zoo.getData("/request/enroll/" + child, true, null);
                     String data = new String(bdata, "UTF-8");
                     if (data.equals(new String("-1"))) {
-                        int version_request = zoo.exists("/request/enroll", true).getVersion();
+                        int version_request = zoo.exists("/request/enroll" + child, true).getVersion();
                         try {
-                            Stat stat_is_register = zoo.exists("/registry/" + child, true);
-                            if (stat_is_register == null) {
+                            Stat stat_registry = zoo.exists("/registry/" + child, true);
+                            if (stat_registry == null) {
                                 zoo.create("/registry/" + child, null, ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
                                 zoo.setData("/request/enroll/" + child, "1".getBytes(), version_request);
                             }
@@ -83,13 +83,44 @@ public class ZooManager implements Runnable {
                         }
                     }
                 }
-            } catch (KeeperException e) {
+            } catch (Exception e ) {
                 e.printStackTrace();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }  
-            catch (Exception e ) {
+            }   
+        }
+    }
+
+    public void quit () throws KeeperException, InterruptedException {
+        Stat stat_exist = zoo.exists("/request/quit", true) ;
+        if (stat_exist != null) {
+            try {
+                List<String> children;
+                QuitManagerWatcher qw = new QuitManagerWatcher(this);
+                Thread qwThread = new Thread(qw);
+                qwThread.setName("quit watcher");
+                qwThread.start();
+                children = zoo.getChildren("/request/quit", qw);
+                for (int i = 0; i < children.size(); i++) {
+                    String child = children.get(i);
+                    byte[] bdata = zoo.getData("/request/quit/" + child, true, null);
+                    String data = new String(bdata, "UTF-8");
+                    if (data.equals("-1")) {
+                        int version_request = zoo.exists("/request/quit" + child, true).getVersion();
+                        try {
+                            Stat stat_registry = zoo.exists("/registry/" + child, true);
+                            if (stat_registry != null) {
+                                zoo.delete("/registry/" + child, stat_registry.getVersion());
+                                zoo.setData("/request/quit/" + child, "1".getBytes(), version_request);
+                            }
+                            else {
+                                zoo.setData("/request/quit/" + child, "2".getBytes(), version_request);                                
+                            }
+                        } catch (KeeperException e){
+                            zoo.setData("/request/quit/" + child, "0".getBytes(), version_request);
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            } catch (Exception e ) {
                 e.printStackTrace();
             }   
         }
