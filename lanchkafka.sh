f92157cb52c9251f0c28698508f48b8fe7dc51c8
Unknown <puccio-94@live.it>
2019-01-31 22:26:52 +0100

    screenshots

--- lanchkafka.sh
+++ lanchkafka.sh
@@ -1,8 +1,7 @@
 zkServer.sh start myconfig.cfg
 sleep 1
-cd kafka/
-bin/kafka-server-start.sh config/server-0.properties &
+~/kafka/bin/kafka-server-start.sh ~/kafka/config/server-0.properties &
 sleep 1
-bin/kafka-server-start.sh config/server-1.properties &
+~/kafka/bin/kafka-server-start.sh ~/kafka/config/server-1.properties &
 sleep 1
-bin/kafka-server-start.sh config/server-2.properties &
+~/kafka/bin/kafka-server-start.sh ~/kafka/config/server-2.properties &
